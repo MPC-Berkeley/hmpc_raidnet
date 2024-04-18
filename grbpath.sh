2377b908450b4919601d4ca40e8c475c688fce73
hansungkim98122 <89807906+hansungkim98122@users.noreply.github.com>
2024-04-18 14:10:18 -0700

    Update grbpath.sh

--- grbpath.sh
+++ grbpath.sh
@@ -1,3 +1,4 @@
+#EDIT the paths to appropriate paths in your local machine
 export GUROBI_HOME="/home/mpc/SMPC_Carla/gurobi1002/linux64"
 export GRB_LICENSE_FILE="/home/mpc/SMPC_Carla/gurobi1002/gurobi.lic"
 export GUROBI_VERSION="100"
